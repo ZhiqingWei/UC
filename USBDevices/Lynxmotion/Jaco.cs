d7edc2d9717440f43cb99147a435e62870dcef22
akkaraphonphan <43610413+akkaraphonphan@users.noreply.github.com>
2020-03-18 16:18:28 +0000

    updated jaco.cs to include speeds

--- USBDevices/Lynxmotion/Jaco.cs
+++ USBDevices/Lynxmotion/Jaco.cs
@@ -1,4 +1,4 @@
-﻿//  BuddyHub Universal Controller
+//  BuddyHub Universal Controller
 //
 //  Created by Zhiqing Wei, 2019
 //  https://github.com/ZhiqingWei/UC
@@ -11,7 +11,7 @@ using System.Text;
 using System.Threading.Tasks;
 using UCProtocol;
 
-namespace Lynxmotion
+namespace Lynxmotion    
 {
     [Export(typeof(UCProtocol.IDevice))]
     public class Jaco : IDevice
@@ -41,90 +41,90 @@ namespace Lynxmotion
         {
         }
 
-        public string Forward(string Mode)
+        public string Forward(string Mode, string Speed)
         {
             switch (Mode)
             {
                 case "arm":
-                    return "Jaco moving forward!! (Arm mode)";
+                    return "Jaco moving forward  at "+ Speed+  " speed !!(Arm mode)";
                 case "wrist":
-                    return "Jaco moving forward!! (Wrist mode)";
+                    return "Jaco moving forward at " + Speed + " speed !!(Wrist mode)";
                 default:
-                    return "Jaco moving forward!!";
+                    return "Jaco moving forward at " + Speed + " speed !!";
             }
  
         }
 
-        public string Backward(string Mode)
+        public string Backward(string Mode, string Speed)
         {
             switch (Mode)
             {
                 case "arm":
-                    return "Jaco moving backward!! (Arm mode)";
+                    return "Jaco moving backward at " + Speed + " speed !! (Arm mode)";
                 case "wrist":
-                    return "Jaco moving backward!! (Wrist mode)";
+                    return "Jaco moving backward at " + Speed + " speed !! (Wrist mode)";
                 default:
-                    return "Jaco moving backward!!";
+                    return "Jaco moving backward at " + Speed + " speed!!";
             }
         }
 
-        public string TiltUp(string Mode)
+        public string TiltUp(string Mode, string Speed)
         {
             switch (Mode)
             {
                 case "arm":
-                    return "Jaco moving UP!! (Arm mode)";
+                    return "Jaco moving UP at " + Speed + " speed!!";
                 case "wrist":
-                    return "Jaco moving UP!! (Wrist mode)";
+                    return "Jaco moving UP!! at  " + Speed + " speed!!  (Wrist mode)";
                 case "finger":
-                    return "Jaco opening three fingers! (Finger mode)";
+                    return "Jaco opening three fingers! at " + Speed + " speed!! (Finger mode)";
                 default:
-                    return "Jaco moving UP!!";
+                    return "Jaco moving UP at " + Speed + "speed!! ";
             }
         }
 
-        public string TiltDown(string Mode)
+        public string TiltDown(string Mode, string Speed)
         {
             switch (Mode)
             {
                 case "arm":
-                    return "Jaco moving DOWN!! (Arm mode)";
+                    return "Jaco moving DOWN at" + Speed + " speed!!";
                 case "wrist":
-                    return "Jaco moving DOWN!! (Wrist mode)";
+                    return "Jaco moving DOWN at " + Speed + " speed!! (Wrist mode)";
                 case "finger":
-                    return "Jaco closing three fingers! (Finger mode)";
+                    return "Jaco closing three fingers! at " + Speed + " speed!!  (Finger mode)";
                 default:
-                    return "Jaco moving DOWN!!";
+                    return "Jaco moving DOWN!! at  " + Speed + " speed!!";
             }
         }
 
-        public string TurnLeft(string Mode)
+        public string TurnLeft(string Mode, string Speed)
         {
             switch (Mode)
             {
                 case "arm":
-                    return "Jaco turning left!! (Arm mode)";
+                    return "Jaco turning left at " + Speed + " speed!!";
                 case "wrist":
-                    return "Jaco turning left!! (Wrist mode)";
+                    return "Jaco turning left at " + Speed + " speed!! (Wrist mode)";
                 case "finger":
-                    return "Jaco opening two fingers! (Finger mode)";
+                    return "Jaco opening two fingers at  " + Speed + " speed!! (Finger mode)";
                 default:
                     return "Jaco turning left!!";
             }
         }
                     
-        public string TurnRight(string Mode)
+        public string TurnRight(string Mode, string Speed)
         {
             switch (Mode)
             {
                 case "arm":
-                    return "Jaco turning right!! (Arm mode)";
+                    return "Jaco turning right at " + Speed + " speed!!";
                 case "wrist":
-                    return "Jaco turning right!! (Wrist mode)";
+                    return "Jaco turning right at " + Speed + " speed!! (Wrist mode)";
                 case "finger":
-                    return "Jaco closing two fingers! (Finger mode)";
+                    return "Jaco closing two fingers! at " + Speed + " speed!!(Finger mode)";
                 default:
-                    return "Jaco turning right!!";
+                    return "Jaco turning right at " + Speed + " speed ";
             }
         }
 
